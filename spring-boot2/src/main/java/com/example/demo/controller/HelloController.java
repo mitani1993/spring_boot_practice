6b80bc47b29b648fca702602b09e5d956a7079dd
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-04-29 09:39:47 +0900

    RequestParamをfalseに変更する

--- spring-boot2/src/main/java/com/example/demo/controller/HelloController.java
+++ spring-boot2/src/main/java/com/example/demo/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
 	}
 	
 	@GetMapping("/greeting")
-	public String greeting(@RequestParam("message") String message, Model model) {
+	public String greeting(@RequestParam(required = false) String message, Model model) {
 		model.addAttribute("sample", message);
 		
 		return "hello";
