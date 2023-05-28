0d16d4b882543e138279f5ac7f263c46bde91a67
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-05-28 17:58:53 +0900

    create spring-ioc-aop

---
+++ spring-ioc-aop/src/main/java/com/example/demo/SpringIocAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIocAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIocAopApplication.class, args);
+	}
+
+}
