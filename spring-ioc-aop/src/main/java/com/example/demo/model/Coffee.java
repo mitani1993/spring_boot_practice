da9980cc5888116630f393225e3a57e4930dac61
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-05-28 18:13:57 +0900

    Create Coffee.java

---
+++ spring-ioc-aop/src/main/java/com/example/demo/model/Coffee.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Coffee {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+}
