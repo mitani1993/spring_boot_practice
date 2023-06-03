9b09f2f0c6ade4aec1120a6a3c553703bd737860
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-06-03 13:17:02 +0900

    Create DataLoader.java

---
+++ spring-security２/src/main/java/com/example/demo/config/DataLoader.java
@@ -0,0 +1,28 @@
+package com.example.demo.config;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.SiteUser;
+import com.example.demo.repository.SiteUserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader implements ApplicationRunner {
+	private final PasswordEncoder passwordEncoder;
+	private final SiteUserRepository userRepository;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		// "admin"ユーザーを用意します
+		var user = new SiteUser();
+		user.setUsername("admin");
+		user.setPassword(passwordEncoder.encode("password"));
+		
+		userRepository.save(user);
+	}
+}
