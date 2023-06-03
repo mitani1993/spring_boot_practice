c7d334649792a3ddca21209b5aa56ecf7cc8ca2a
mitani1993 <69582233+mitani1993@users.noreply.github.com>
2023-06-03 15:34:06 +0900

    Update SiteUser.java

--- spring-security２/src/main/java/com/example/demo/model/SiteUser.java
+++ spring-security２/src/main/java/com/example/demo/model/SiteUser.java
@@ -1,9 +1,14 @@
 package com.example.demo.model;
 
+import com.example.demo.util.Authority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +20,17 @@ public class SiteUser {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Size(min = 2, max = 20)
 	private String username;
+	
+	@Size(min = 4, max = 255)
 	private String password;
+	
+	@NotBlank
+	@Email
+	private String email;
+	
+	private int gender;
+	private boolean admin;
+	private Authority authority;
 }
